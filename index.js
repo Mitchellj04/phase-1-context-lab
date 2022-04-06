f6c51b3586c942fbe0bb5f472d97d794a7a2d793
JustinMitchell <mitchelljm04@gmail.com>
2022-04-05 21:17:26 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -8,6 +8,53 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+const createEmployeeRecord = (recArray) => {
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (recordsArray) => {
+    return recordsArray.map(rec => createEmployeeRecord(rec))
+
+}
+
+const createTimeInEvent = function(dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    const inEvents = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date 
+    }
+    this.timeInEvents.push(inEvents)
+    return this
+}
+
+const createTimeOutEvent = function(dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    const outEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(outEvent)
+    return this 
+}
+
+const hoursWorkedOnDate = function(targetDate){
+   const inEvent =  this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+   const outEvent = this.timeOutEvents.find(outEvent => outEvent.date === targetDate)
+   return (outEvent.hour - inEvent.hour) / 100
+}
+
+const wagesEarnedOnDate = function(targetDate){
+    return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +68,12 @@ const allWagesFor = function () {
     return payable
 }
 
+const findEmployeeByFirstName = function(srcArray, firstName){
+    return srcArray.find(rec => rec.firstName === firstName)
+}
+
+const calculatePayroll = function(recArray){
+    return recArray.reduce((total, rec) => {
+        return total + allWagesFor.call(rec)
+    }, 0)
+}
